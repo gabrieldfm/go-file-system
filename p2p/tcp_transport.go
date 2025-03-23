f71c659b78704298986348774993eb19497d0ab6
gabrieldfm <gabrieldfm13@gmail.com>
2025-03-23 19:39:35 -0300

    add listen and accept, unit test

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,4 +1,62 @@
 package p2p
 
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type TCPPeer struct {
+	conn     net.Conn
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
 type TCPTransport struct {
+	listeAddress string
+	listener     net.Listener
+
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listeAddress: listenAddr,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+	t.listener, err = net.Listen("tcp", t.listeAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accep error: %s\n", err)
+		}
+
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	peer := NewTCPPeer(conn, true)
+
+	fmt.Printf("new incoming peer %+v\n", peer)
 }
